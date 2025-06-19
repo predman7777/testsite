424b00c5d7b6105c1a53d84a6a3c240373449683
Pamela Redman <pamelam1@Pamela-M1-Pro.local>
2025-06-18 20:23:25 -0700

    add from cursor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import app from './firebase';
 
 function App() {
+  useEffect(() => {
+    console.log('Firebase App Name:', app.name); // should be '[DEFAULT]'
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Firebase!</h1>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArrCKxTU4CbS6S1Wa0iQoF_5W-57NeoC0",
+  authDomain: "testsitefirebase.firebaseapp.com",
+  projectId: "testsitefirebase",
+  storageBucket: "testsitefirebase.firebasestorage.app",
+  messagingSenderId: "102990399802",
+  appId: "1:102990399802:web:8cb0b762f4ead1a6516803"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
